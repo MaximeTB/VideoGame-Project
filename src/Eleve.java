18a7d2e54c2f029b27ce7e39e4c22a6d013553a9
Phobos35 <y.turbe@laposte.net>
2021-10-19 14:38:10 +0200

    class Eleve

---
+++ src/Eleve.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Eleve {
+    private String name;
+    private Integer etude;
+    private Integer fatigue;
+    private Integer invest;
+    //private ArrayList<motcle>;
+
+
+    public Integer getEtude() {
+        return etude;
+    }
+
+    public Integer getFatigue() {
+        return fatigue;
+    }
+
+    public Integer getInvest() {
+        return invest;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Eleve(){
+
+    }
+}
