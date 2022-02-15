0144f417649be7594961a607a10f615379dc63ed
Arthur <arthur.hubert1@gmail.com>
2022-02-15 14:27:10 +0100

    update 01/02, début de code des poles et lieux

--- src/Pole.java
+++ src/Pole.java
@@ -2,42 +2,16 @@ import java.util.ArrayList;
 
 public class Pole {
     String name;
-    ArrayList<Eleve> Member=new ArrayList<Eleve>();
+    ArrayList<Eleve> Member=null;
     private int XP=0;
     private int level=1;
-    private String color;
 
-    public Pole(String name, String color) {
+    public Pole(String name) {
         this.name=name;
-        this.color=color;
-
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public ArrayList<Eleve> getMember() {
-        return Member;
     }
 
     public void addMember(Eleve e){
-        Member.add(e);
+        this.Member.add(e);
     }
     public void levelUp(){this.level++;}
-
-    public void meeting(){
-        int bonus=0;
-        for (Eleve eleve : Member){
-            for (Skills skill : eleve.getSkillsList()){
-                if (skill.getColor().equals(color)){
-                    bonus++;
-                }
-
-            }
-        }
-        level+=1+bonus;
-    }
-
-
 }
