bc4435bd5a777f89b85530cc7522aa19aeb4ee2a
Phobos35 <y.turbe@laposte.net>
2022-02-15 17:01:27 +0100

    commit

---
+++ src/Bureau.java
@@ -0,0 +1,35 @@
+public class Bureau extends Pole{
+    private Eleve prez, trez, secr;
+
+
+    public Bureau(String name, String color1){
+        super(name, color1);
+    }
+
+    public void giveRole(String role, Eleve eleve){
+        if(role.equals("Trésorier")){
+            trez=eleve;
+        }
+
+        if(role.equals("Président")){
+            prez=eleve;
+        }
+
+        if(role.equals("Secrétaire")){
+            secr=eleve;
+        }
+
+    }
+
+    public Eleve getPrez() {
+        return prez;
+    }
+
+    public Eleve getSecr() {
+        return secr;
+    }
+
+    public Eleve getTrez() {
+        return trez;
+    }
+}
