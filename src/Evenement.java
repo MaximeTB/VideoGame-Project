daf3abbdcd25e15440a8c7123f74a4b94874706b
MaximeTB <92383989+MaximeTB@users.noreply.github.com>
2022-05-08 21:45:53 +0200

    Max

--- src/Evenement.java
+++ src/Evenement.java
@@ -2,18 +2,30 @@ import java.sql.Array;
 import java.util.ArrayList;
 
 public abstract class Evenement{
+    /**
+     * Nom de l'Evenement
+     */
     protected String Name; //Nom de l'événèment
-    protected int niveau;
-    protected String Type; //vaut J ou N si c'est un lieu (nuit ou jour) et vaut G si c'est un effet global
-    protected String Tier; //1,2,3 ou liste -> stade d'apparition minimal de l'event
+    /**
+     * Tier de l'Evenement , 1 2 ou 3. Plus le Tier de l'Evenement est eleve
+     * plus son impact sur le Liste , les Eleves et leurs stats est important et il apparait tard dans la partie.
+     */
+    protected int Tier;
+    /**
+     * Jour "J" ou Nuit "N".
+     */
+    protected String Type;//vaut J ou N si c'est un lieu (nuit ou jour) et vaut G si c'est un effet global
+    /**
+     * Nombre de tours que dure l'Evenement.
+     */
     protected int duree;
 
 
 
 //constructeur
-    public Evenement(String Name,int niveau,String Type,int duree){
+    public Evenement(String Name,int Tier,String Type,int duree){
         this.Name=Name;
-        this.niveau=niveau;
+        this.Tier=Tier;
         this.Type=Type;
         this.duree=duree;
     }
@@ -23,14 +35,19 @@ public abstract class Evenement{
     public String getName() {
         return Name;
     }
-    public int getNiveau() {
-        return niveau;
+    public int getTier() {
+        return Tier;
     }
     public String getType() {
         return Type;
     }
 
 //abstract methode
+
+    /**
+     * Cette methode s'appliquera de plusieurs manières en fonction du type d'evenement.
+     * @param game
+     */
     public abstract void applyEvent(Game game);
 
 
@@ -38,7 +55,7 @@ public abstract class Evenement{
     @Override
     public String toString() {
         return  "\n {Name='" + Name + '\'' +
-                ", niveau=" + niveau +
+                ", niveau=" + Tier +
                 ", Type='" + Type + '\'' +
                 "}";
     }
--- src/EventSurLieu.java
+++ src/EventSurLieu.java
@@ -1,15 +1,20 @@
+/**
+ * Ce type d'Evenement cree un Lieu pour un duree limitee. Par exemple l'Evenement Soiree cree un Lieu de Nuit Soiree.
+ */
 public class EventSurLieu extends Evenement{
     private int Duree;
 
     //type d'evenement qui modifie, créer ou désactive un lieu
-    public EventSurLieu(String Name, int niveau, int duree) {
-        super(Name, niveau, "N",duree);
+    public EventSurLieu(String Name, int Tier,String Type, int duree) {
+        super(Name, Tier, Type,duree);
     }
 
 
     @Override
+    /**
+     * L'application de cet Event a pour Effet de creer un Lieu avec les stats correspondantes aux stats de l'Event.
+     */
     public void applyEvent(Game game) {
-
     }
 
 }
